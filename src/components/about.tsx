94547ddb4ef6f6159c511165da0de0ddcfaca17f
GhazanfarShahbaz <ghazanfarshahbaz2409@gmail.com>
2024-07-08 20:47:19 -0400

    fix typo

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,7 +2,7 @@ import MarkdownProcessor from "./markdown_processor";
 
 function AboutPage() {
     return (
-        <MarkdownProcessor markdown="# This about page is in construciton" />
+        <MarkdownProcessor markdown="# This about page is in construction" />
     );
 }
 
